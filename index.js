26c77dd63818a2b33809103a5190c135789b4277
anaguedes19 <anajuliabernardo19@gmail.com>
2021-04-01 11:15:58 -0300

    atualizacao

--- index.js
+++ index.js
@@ -33,25 +33,41 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
-  const params = request.params;
-
-  console.log(params)
+  const {id} = request.params; //aqui pegamos o id
+  const {title, owner} = request.body; // retornar uma nova informação
   
-  return response.json([
-    'Projeto 60',
-    'Projeto 2',
-    'Projeto 3',
-    'Projeto 4',
-    'Projeto 5',
-   ])
+  const projectIndex = projects.findIndex(project => project.id === id);
+  
+  if (projectIndex < 0) {
+      return response.status(400).json({error: 'Projeto não encontrado'});
+  }
+
+  // agora que tenho indice vou criar uma nova informação do projeto 
+
+  const project = { 
+    id, 
+    title,
+    owner,
+  }
+
+projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-   'Projeto 60',
-   'Projeto 2',
-  
-  ]);
+ const {id} = request.params
+
+ const projectIndex = projects.findIndex(project => project.id === id);
+
+ if (projectIndex < 0) {
+  return response.status(400).json({error: 'Projeto não encontrado'});
+}
+
+projects.splice(projectIndex, 1);
+
+ 
+  return response.status(204).send();
 });
 
 app.listen(3000, () => {
